b9930895e51b42c578ed63d7a82fdc9368db6753
wydsolo <central@soloo.win>
2026-01-14 18:13:32 -0500

    ads

---
+++ static/solo/a3j4vtm9qs9ef.js
@@ -0,0 +1,16 @@
+<script>
+(function () {
+  const scripts = [
+    "https://cuddlecoveragehiss.com/a5/a3/40/a5a340bb6545fc1a5580d64834578a0c.js", // qatual
+    "https://pl28069434.effectivegatecpm.com/bf/ca/7c/bfca7cfeaaba489120f1b12ef7c822cb.js" // fentezzys
+  ];
+
+  const randomScript = scripts[Math.floor(Math.random() * scripts.length)];
+
+  const s = document.createElement("script");
+  s.src = randomScript;
+  s.async = true;
+
+  document.head.appendChild(s);
+})();
+</script>
